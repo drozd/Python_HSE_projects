38d06fee18388bce0b8bca52637d4616b3191544
Nadya_Motina <thatdau@gmail.com>
2015-03-21 15:13:57 +0300

    added separation by ! and removed blank lines in output

--- simple_splitter.py
+++ simple_splitter.py
@@ -10,7 +10,10 @@ def splitter(text):
         >>> splitter("Маша уронила мячик. Мячик уплыл.")
         ['Маша уронила мячик.', 'Мячик уплыл.']
         """
-        sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s', text)
+        sentences = re.split(r'(?<!\w\.\w.)(?<![A-ZА-Я][a-zа-я]\.)(?<=\.|\?|\!)\s', text)
+        for s in sentences:
+                if s=="" or s==" ":
+                        sentences.pop(sentences.index(s))
         return sentences
 
 sentences = splitter(text)
@@ -18,34 +21,24 @@ sentences = splitter(text)
 print("Here are the splitted sentences: \n")
 for s in sentences:
         print(s)
-        print("\n")
 
-"""
-Sentences for testing:
-Mr. Smith bought cheapsite.com for 1.5 million dollars, i.e. he paid a lot for it.
-Did he mind? Adam Jones Jr. thinks he didn't. In any case, this isn't true...
-Well, with a probability of .9 it isn't.
-"""
+##Explanation for regex: (?<!\w\.\w.)(?<![A-ZА-Я][a-zа-я]\.)(?<=\.|\?|\!)\s
 
-##Explanation:
-##"(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?)\s"
-
-##(?<!\w\.\w.) Negative Lookbehind - Assert that it is impossible to match the regex below
+##(?<!\w\.\w.) Negative Lookbehind =
+##              Matches if the current position in the string is not preceded by a match for
+##              \w\.\w. This is called a negative lookbehind assertion.
 ##\w match any word character [a-zA-Z0-9_]
 ##\. matches the character . literally
 ##\w match any word character [a-zA-Z0-9_]
 ##. matches any character (except newline)
 
-##(?<![A-Z][a-z]\.) Negative Lookbehind - Assert that it is impossible to match the regex below
-##[A-Z] match a single character present in the list below
-##A-Z a single character in the range between A and Z (case sensitive)
-##[a-z] match a single character present in the list below
-##a-z a single character in the range between a and z (case sensitive)
+##(?<![A-ZА-Я][a-zа-я]\.) Negative Lookbehind 
+##[A-ZА-Я] match a single character present in the list below
+##[a-zа-я] match a single character present in the list below
 ##\. matches the character . literally
 
-##(?<=\.|\?) Positive Lookbehind - Assert that the regex below can be matched
+##(?<=\.|\?|/!) Positive Lookbehind
 ##1st Alternative: \.
-##\. matches the character . literally
 ##2nd Alternative: \?
-##\? matches the character ? literally
+##3rd Alternative: \!
 ##\s match any white space character [\r\n\t\f ]
